96fb34475919ff1e96b89a6a4bc6807419a55286
montoyaguzman <montoyaguzman7@gmail.com>
2022-07-23 12:10:11 -0500

    feat: agrega configuracion de cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // 1 Importaciones de componentes necesarios
 const express = require('express');
 const colors = require('colors');
+
+// CORS
+const cors = require('cors');
+
 // 2 Declaración de variables para levantar el servidor
 const app = express();
 const port = 3000;
@@ -8,6 +12,26 @@ const port = 3000;
 const routerApi = require('./routes/main.controller');
 // 4 Agregar middleware para uso de req.body
 app.use(express.json());
+
+
+// CORS
+const whitelist = ['http://127.0.0.1:5501'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido por tema de CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
+
+// ROOT ENDPOINT
+app.get('/', (req, res) => {
+    res.send('Api shoes by montoyitag')
+});
+
 // 5 Levantar el servidor
 app.listen(port, () => {
     console.log('Servidor express listening...'.rainbow);
